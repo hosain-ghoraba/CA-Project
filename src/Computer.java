4f30834713f48c9505fa2bb6e15e2b6014d7e8a2
hosain ghoraba <hussain.ghoraba@gmail.com>
2023-05-14 20:57:40 +0300

    some work in parial printings

--- src/Computer.java
+++ src/Computer.java
@@ -8,7 +8,8 @@ public class Computer {
     int[] registerFile; // R0 to R31
     int PC ;
     int currentCycle;
-    int fetchnow;
+    int fetchWaitTime;
+    Instruction[] instructions_already_done_in_pipeline;
     
     Instruction Instruction_in_Fetch_Stage;
     Instruction Instruction_in_Decode_Stage;
@@ -17,9 +18,11 @@ public class Computer {
     Instruction Instruction_in_Writeback_Stage;
     //int[] fetch;
     //int[] decode;
-    int[] execute;
-    int[] memorystage;
-    int[] wb;
+    int[] execute_Stage_Inputs;
+    int[] memory_Stage_Inputs;
+    int[] writeBack_Stage_Inputs;
+
+
     
 
     public Computer() {
@@ -27,21 +30,18 @@ public class Computer {
         registerFile = new int[32];
         PC = 0;
         currentCycle = 0 ;
-        fetchnow=0;
+        fetchWaitTime = 0;
+        instructions_already_done_in_pipeline = new Instruction[5];
       //  fetch=new int[5];
        // decode=new int[5];
-        memorystage=new int[3];
-        execute=new int[5];
-        wb=new int[3];
-
-
-      
+        memory_Stage_Inputs=new int[3];
+        execute_Stage_Inputs=new int[5];
+        writeBack_Stage_Inputs=new int[3];
     }
 
     private void run(String filePath) throws ComputerException{
         loadProgramIntoMemory(filePath);
-        int maxClocks = 7 + ( (instructions_count_in_memory-1) * 2 );
-        
+        int maxClocks = 7 + ( (instructions_count_in_memory-1) * 2 );    
         for(int i = 0 ; i < maxClocks ; i++)
             Tickle_Clock();
         printFinalRequirements();    
@@ -50,8 +50,16 @@ public class Computer {
     }
     private void Tickle_Clock()throws ComputerException {
         currentCycle++;
-        process_PipeLine();      
-        
+        //------------- saving old inputs of each stage, as well as old register file and memory
+        int[] oldInputsOfExecuteStage = memory_Stage_Inputs.clone();
+        int[] oldInputsOfMemoryStage = execute_Stage_Inputs.clone();
+        int[] oldInputsOfWriteBackStage = writeBack_Stage_Inputs.clone();
+        int[] oldRegisterFile = registerFile.clone();
+        int[] oldMemory = memory.clone();     
+        //--------------------------
+        process_PipeLine();     
+        printAfterCycle(oldInputsOfExecuteStage, oldInputsOfMemoryStage, oldInputsOfWriteBackStage, oldRegisterFile, oldMemory);
+
     }   
     private void process_PipeLine() throws ComputerException {
 
@@ -61,10 +69,16 @@ public class Computer {
             if(Instruction_in_Writeback_Stage.timeInStage == 1)
             {
                 Instruction_in_Writeback_Stage.execute_in_WRITEBACK_stage(this);
+                instructions_already_done_in_pipeline[4] = Instruction_in_Writeback_Stage ;
                 Instruction_in_Writeback_Stage = null;
             }
 
+        } 
+        else
+        {
+            instructions_already_done_in_pipeline[4] = null ;
         }
+
         
         if(Instruction_in_Memory_Stage != null )
         {
@@ -72,7 +86,7 @@ public class Computer {
             if(Instruction_in_Memory_Stage.timeInStage == 1)
             {
                 Instruction_in_Memory_Stage.execute_in_MEMORY_stage(this);
-                //should print here
+                instructions_already_done_in_pipeline[3] = Instruction_in_Memory_Stage ;
                 Instruction_in_Writeback_Stage = Instruction_in_Memory_Stage;
                 Instruction_in_Writeback_Stage.timeInStage = 0;
                 Instruction_in_Memory_Stage = null;
@@ -80,13 +94,18 @@ public class Computer {
             }
 
         }
+        else
+        {
+            instructions_already_done_in_pipeline[3] = null ;
+        }
+
         if(Instruction_in_Execute_Stage != null)
         {
             Instruction_in_Execute_Stage.timeInStage++;
             if(Instruction_in_Execute_Stage.timeInStage == 2)
             {
                 Instruction_in_Execute_Stage.execute_in_EXECUTE_stage(this);
-                //should print here
+                instructions_already_done_in_pipeline[2] = Instruction_in_Execute_Stage ;
                 Instruction_in_Memory_Stage = Instruction_in_Execute_Stage;
                 Instruction_in_Memory_Stage.timeInStage = 0;
                 Instruction_in_Execute_Stage = null;
@@ -94,13 +113,19 @@ public class Computer {
             }
 
         }
+        else
+        {
+            instructions_already_done_in_pipeline[2] = null ;
+        }
+
+
         if(Instruction_in_Decode_Stage != null)
         {
             Instruction_in_Decode_Stage.timeInStage++;
             if(Instruction_in_Decode_Stage.timeInStage == 2)
             {
                 Instruction_in_Decode_Stage.execute_in_DECODE_stage(this);
-                //should print here
+                instructions_already_done_in_pipeline[1] = Instruction_in_Decode_Stage ;
                 Instruction_in_Execute_Stage = Instruction_in_Decode_Stage;
                 Instruction_in_Execute_Stage.timeInStage = 0;
                 Instruction_in_Decode_Stage = null;
@@ -108,39 +133,30 @@ public class Computer {
             }
 
         }
-        
-        
-       /* if(Instruction_in_Fetch_Stage != null)
-        {
-            Instruction_in_Fetch_Stage.timeInStage++;
-            if(Instruction_in_Fetch_Stage.timeInStage == 2)
-            {
-                Instruction_in_Fetch_Stage.execute_in_FETCH_stage(this);
-                Instruction_in_Decode_Stage = Instruction_in_Fetch_Stage;
-                Instruction_in_Decode_Stage.timeInStage = 0;
-                Instruction_in_Fetch_Stage = null;
-
-            }
-
-        }
         else
         {
+            instructions_already_done_in_pipeline[1] = null ;
+        }
+
+
+        if(fetchWaitTime==0) 
+        {
+            fetchWaitTime=1;
             Instruction_in_Fetch_Stage = fetchNextInstruction();
-      0      Instruction_in_Fetch_Stage.timeInStage++;
-        }*/
-        
-        if(fetchnow==0) {
-            fetchnow=1;
-            Instruction_in_Fetch_Stage = fetchNextInstruction();
-            //should print here
+            instructions_already_done_in_pipeline[0] =  Instruction_in_Fetch_Stage ;
             Instruction_in_Decode_Stage=Instruction_in_Fetch_Stage;
             Instruction_in_Fetch_Stage=null;
 
         }
-        else {fetchnow--;}
+        else 
+        {
+            fetchWaitTime--;
+            instructions_already_done_in_pipeline[0] = null ;
+        }
+        
         
     }
-    public Instruction fetchNextInstruction() {// just reads the next instruction to be fetched without incrementing the PC , pc is incremented in the fetch methods
+    public Instruction fetchNextInstruction() {
         if(PC >= instructions_count_in_memory)
             return null;
         return new Instruction(memory[PC++]);    
@@ -155,5 +171,74 @@ public class Computer {
     }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    }
+    // to be continued
+    public void printAfterCycle(int[] oldInputsOfExecuteStage, int[] oldInputsOfMemoryStage, int[] oldInputsOfWritebackStage, int[] oldRegisterFile, int[] oldMemory) throws ComputerException{
+    
+        // cycle number
+        System.out.println("cycle number : " + currentCycle); // cycle number
+        // Which instruction is being executed at each stage
+        System.out.println("instruction in fetch stage : " + instructions_already_done_in_pipeline[0]); 
+        System.out.println("instruction in decode stage : " + instructions_already_done_in_pipeline[1]);
+        System.out.println("instruction in execute stage : " + instructions_already_done_in_pipeline[2]);
+        System.out.println("instruction in memory stage : " + instructions_already_done_in_pipeline[3]);
+        System.out.println("instruction in writeback stage : " + instructions_already_done_in_pipeline[4]);
+        // What are the input parameters/values for each stage
+        if(instructions_already_done_in_pipeline[0] != null)
+            System.out.println("fetch stage input parameters: PC = " + (PC-1));
+        if(instructions_already_done_in_pipeline[1] != null)
+            System.out.println("decode stage input parameters: instruction = " + instructions_already_done_in_pipeline[1]);
+        if(instructions_already_done_in_pipeline[2] != null)
+        {
+            if(instructions_already_done_in_pipeline[2].getType().equals("R"))
+            {
+
+            }
+            else if(instructions_already_done_in_pipeline[2].getType().equals("I"))
+            {
+
+            }
+            else if(instructions_already_done_in_pipeline[2].getType().equals("J"))
+            {
+
+            }
+        }
+        if(instructions_already_done_in_pipeline[3] != null)
+        {
+            if(instructions_already_done_in_pipeline[3].getType().equals("R"))
+            {
+
+            }
+            else if(instructions_already_done_in_pipeline[3].getType().equals("I"))
+            {
+
+            }
+            else if(instructions_already_done_in_pipeline[3].getType().equals("J"))
+            {
+
+            }
+        }
+        if(instructions_already_done_in_pipeline[4] != null)
+        {
+            if(instructions_already_done_in_pipeline[4].getType().equals("R"))
+            {
+
+            }
+            else if(instructions_already_done_in_pipeline[4].getType().equals("I"))
+            {
+
+            }
+            else if(instructions_already_done_in_pipeline[4].getType().equals("J"))
+            {
+
+            }
+        }
+        // changes in registerFile and memory
+        for(int i = 0 ; i < registerFile.length ; i++)
+            if(registerFile[i] != oldRegisterFile[i])
+                System.out.println("R" + i + "changed from" + oldRegisterFile[i] + " to " + registerFile[i]);
+        for(int i = 0 ; i < memory.length ; i++)
+            if(memory[i] != oldMemory[i])
+                System.out.println("memory " + "[" + i + "] " + "changed from " + oldMemory[i] + " to " + memory[i]);
     }
 }   
--- src/Instruction.java
+++ src/Instruction.java
@@ -18,30 +18,10 @@ public  class Instruction {
             computer.registerFile[index] = value;
     }
 
-    public void execute_in_FETCH_stage(Computer computer) throws ComputerException{
-         //computer.Instruction_in_Fetch_Stage=new Instruction(computer.memory[computer.PC]);
-         //computer.PC++;
-       
-    	/*switch(getopcode()) {
-            case 0: FETCH_ADD(computer); break;
-            case 1: FETCH_SUB(computer); break;
-            case 2: FETCH_MUL(computer); break;
-            case 3: FETCH_MOVI(computer); break;
-            case 4: FETCH_JEQ(computer); break;
-            case 5: FETCH_AND(computer); break;
-            case 6: FETCH_XORI(computer); break;
-            case 7: FETCH_JMP(computer); break;
-            case 8: FETCH_LSL(computer); break;
-            case 9: FETCH_LSR(computer); break;
-            case 10: FETCH_MOVR(computer); break;
-            case 11: FETCH_MOVM(computer); break;
-            default: throw new ComputerException("opcode for instruction " + value + " is not valid");
-        }*/
-    }
     public void execute_in_DECODE_stage(Computer computer)throws ComputerException{
         int r1,r2,r3,shamt,imm,add;
     	int opcode=computer.Instruction_in_Decode_Stage.value>>28;
-    	computer.execute[0]= opcode;
+    	computer.execute_Stage_Inputs[0]= opcode;
     	switch(opcode) {
             case 0: case 1: case 2: case 5:case 8:case 9:
             r1= ((value<<4)>>>27);
@@ -49,10 +29,10 @@ public  class Instruction {
             r3=(value<<14)>>>27;
             shamt=(value<<19)>>>19;
             
-            computer.execute[1]= r1;
-            computer.execute[2]=r2 ;
-            computer.execute[3]= r3;
-            computer.execute[4]=shamt;
+            computer.execute_Stage_Inputs[1]= r1;
+            computer.execute_Stage_Inputs[2]=r2 ;
+            computer.execute_Stage_Inputs[3]= r3;
+            computer.execute_Stage_Inputs[4]=shamt;
             break;
             
             case 3: case 4: case 6: case 10 : case 11:
@@ -60,16 +40,16 @@ public  class Instruction {
             	r2=(value<<9)>>>27;
             	imm=(value<<14)>>14;
             	 
-                 computer.execute[1]= r1;
-                 computer.execute[2]=r2 ;
-                 computer.execute[3]= imm;
+                 computer.execute_Stage_Inputs[1]= r1;
+                 computer.execute_Stage_Inputs[2]=r2 ;
+                 computer.execute_Stage_Inputs[3]= imm;
                  break;
                  
             
             case 7: 
             	add=(value<<4)>>4;
             	
-                computer.execute[1]= add;
+                computer.execute_Stage_Inputs[1]= add;
                 break;
             
            
@@ -78,37 +58,37 @@ public  class Instruction {
     }
     public void execute_in_EXECUTE_stage(Computer computer)throws ComputerException{
     	int res;
-    	computer.memorystage[0]=computer.execute[0];
-    	switch(computer.execute[0]) {
+    	computer.memory_Stage_Inputs[0]=computer.execute_Stage_Inputs[0];
+    	switch(computer.execute_Stage_Inputs[0]) {
         
     	case 0: 
-        res=getRegister(computer.execute[2], computer) + getRegister(computer.execute[3], computer);
-        computer.memorystage[1]=computer.execute[1];
-        computer.memorystage[2]=res;
+        res=getRegister(computer.execute_Stage_Inputs[2], computer) + getRegister(computer.execute_Stage_Inputs[3], computer);
+        computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+        computer.memory_Stage_Inputs[2]=res;
         break;
         
         case 1: 
-        res=getRegister(computer.execute[2], computer) -getRegister(computer.execute[3], computer);
-        computer.memorystage[1]=computer.execute[1];
-        computer.memorystage[2]=res;
+        res=getRegister(computer.execute_Stage_Inputs[2], computer) -getRegister(computer.execute_Stage_Inputs[3], computer);
+        computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+        computer.memory_Stage_Inputs[2]=res;
         break;
         
         case 2: 
-        res=getRegister(computer.execute[2], computer) * getRegister(computer.execute[3], computer);
-        computer.memorystage[1]=computer.execute[1];
-        computer.memorystage[2]=res;
+        res=getRegister(computer.execute_Stage_Inputs[2], computer) * getRegister(computer.execute_Stage_Inputs[3], computer);
+        computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+        computer.memory_Stage_Inputs[2]=res;
         break;
         
         case 3: 
-        res=computer.execute[3];
-        computer.memorystage[1]=computer.execute[1];
-        computer.memorystage[2]=res;
+        res=computer.execute_Stage_Inputs[3];
+        computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+        computer.memory_Stage_Inputs[2]=res;
 
         break;
         
         case 4:
-        if(getRegister(computer.execute[1], computer)==getRegister(computer.execute[2], computer)) {
-        	computer.PC=computer.PC+computer.execute[3];
+        if(getRegister(computer.execute_Stage_Inputs[1], computer)==getRegister(computer.execute_Stage_Inputs[2], computer)) {
+        	computer.PC=computer.PC+computer.execute_Stage_Inputs[3];
         	
         	computer.Instruction_in_Fetch_Stage=null;
             computer.Instruction_in_Decode_Stage=null;
@@ -116,72 +96,72 @@ public  class Instruction {
         	
         	 break;
         case 5:
-        res=getRegister(computer.execute[2], computer) & getRegister(computer.execute[3], computer);
-        computer.memorystage[1]=computer.execute[1];
-        computer.memorystage[2]=res;
+        res=getRegister(computer.execute_Stage_Inputs[2], computer) & getRegister(computer.execute_Stage_Inputs[3], computer);
+        computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+        computer.memory_Stage_Inputs[2]=res;
         break;
         
-        case 6: res=getRegister(computer.execute[2], computer)^computer.execute[3];
-        computer.memorystage[1]=computer.execute[1];
-        computer.memorystage[2]=res;
+        case 6: res=getRegister(computer.execute_Stage_Inputs[2], computer)^computer.execute_Stage_Inputs[3];
+        computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+        computer.memory_Stage_Inputs[2]=res;
         break;
         
         case 7: 
-        computer.PC=(computer.PC>>28)<<28+computer.execute[1];
+        computer.PC=(computer.PC>>28)<<28+computer.execute_Stage_Inputs[1];
         computer.Instruction_in_Fetch_Stage=null;
         computer.Instruction_in_Decode_Stage=null;
-        computer.fetchnow=2;
+        computer.fetchWaitTime=2;
          break;
         
         case 8: 
-        	res=getRegister(computer.execute[2], computer)<<computer.execute[4];
-        	computer.memorystage[1]=computer.execute[1];
-            computer.memorystage[2]=res;
+        	res=getRegister(computer.execute_Stage_Inputs[2], computer)<<computer.execute_Stage_Inputs[4];
+        	computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+            computer.memory_Stage_Inputs[2]=res;
         
         case 9: 
-        	res=getRegister(computer.execute[2], computer)>>>computer.execute[4];
-        	computer.memorystage[1]=computer.execute[1];
-            computer.memorystage[2]=res;; break;
+        	res=getRegister(computer.execute_Stage_Inputs[2], computer)>>>computer.execute_Stage_Inputs[4];
+        	computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+            computer.memory_Stage_Inputs[2]=res;; break;
         
         case 10: 
-        	res=getRegister(computer.execute[2], computer)+computer.execute[3];
-        	computer.memorystage[1]=computer.execute[1];
-            computer.memorystage[2]=res;
+        	res=getRegister(computer.execute_Stage_Inputs[2], computer)+computer.execute_Stage_Inputs[3];
+        	computer.memory_Stage_Inputs[1]=computer.execute_Stage_Inputs[1];
+            computer.memory_Stage_Inputs[2]=res;
         	break;
         
         case 11: 
-        	res=getRegister(computer.execute[2], computer)+computer.execute[3];
-        	computer.memorystage[1]=getRegister(computer.execute[1], computer);
-            computer.memorystage[2]=res;
+        	res=getRegister(computer.execute_Stage_Inputs[2], computer)+computer.execute_Stage_Inputs[3];
+        	computer.memory_Stage_Inputs[1]=getRegister(computer.execute_Stage_Inputs[1], computer);
+            computer.memory_Stage_Inputs[2]=res;
         	break;
         default: throw new ComputerException("opcode for instruction " + value + " is not valid");
     }
     }
    
     public void execute_in_MEMORY_stage(Computer computer)throws ComputerException{
-       computer.wb[0]=computer.memorystage[0];
-    	switch(computer.wb[0]) {
+       computer.writeBack_Stage_Inputs[0]=computer.memory_Stage_Inputs[0];
+    	switch(computer.writeBack_Stage_Inputs[0]) {
             case 0:   case 1:   case 2:   case 3:    case 4:  case 5: case 6:case 7: case 8:  case 9: 
-            	computer.wb[1]=computer.memorystage[1];
-            	computer.wb[2]=computer.memorystage[2];
+            	computer.writeBack_Stage_Inputs[1]=computer.memory_Stage_Inputs[1];
+            	computer.writeBack_Stage_Inputs[2]=computer.memory_Stage_Inputs[2];
             	break;
             case 10:
-            	computer.wb[1]=computer.memorystage[1];
-            	computer.wb[2]=computer.memory[computer.memorystage[2]];
+            	computer.writeBack_Stage_Inputs[1]=computer.memory_Stage_Inputs[1];
+            	computer.writeBack_Stage_Inputs[2]=computer.memory[computer.memory_Stage_Inputs[2]];
             	
             	
             	break;
             case 11:
-            	computer.memory[computer.memorystage[2]]=computer.memorystage[1];
+            	computer.memory[computer.memory_Stage_Inputs[2]]=computer.memory_Stage_Inputs[1];
             	
             	break;
             default: throw new ComputerException("opcode for instruction " + value + " is not valid");
         }
     }
     public void execute_in_WRITEBACK_stage(Computer computer)throws ComputerException{
-        switch(computer.wb[0]) {
+        switch(computer.writeBack_Stage_Inputs[0]) {
             case 0:  case 1: case 2:case 3:case 5:case 6:case 8:case 9:case 10: 
-            	computer.registerFile[computer.wb[1]]=computer.wb[2]; 
+            	computer.registerFile[computer.writeBack_Stage_Inputs[1]]=computer.writeBack_Stage_Inputs[2]; 
             	break;
            
             case 4:case 7: case 11:  break;
@@ -203,4 +183,13 @@ public static void main(String[] args) throws ComputerException {
 	System.out.println(c.registerFile[1]);
 }
 
+public String getType() throws ComputerException {
+    switch(getopcode())
+    {
+        case 0 : case 1 : case 2 : case 5 : case 8 : case 9 : return "R";
+        case 3 : case 4 : case 6 : case 10 : case 11 : return "I";
+        case 7 : return "J";
+        default: throw new ComputerException("opcode for instruction " + value + " is not valid");
+    }
+}
 }
